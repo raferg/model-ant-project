a83b9d7e2f73fba1f1cf5147ecba0d02c1127e84
Rafer <rafer.gluyas@gmail.com>
2023-10-23 13:11:17 +0800

    this should still pass

--- test/TestSortingAlgorithm.java
+++ test/TestSortingAlgorithm.java
@@ -34,7 +34,6 @@ public class TestSortingAlgorithm {
 		int[] actualSortedCollection = SortingAlgorithm.sortInBubbleSort(collection);
 		System.out.println("Data set After Bubble Sorting : \t" + Arrays.toString(actualSortedCollection));
 		assertEquals(Arrays.toString(sortedCollection), Arrays.toString(collection));
-		assertTrue(true);
 		// assertEquals(Arrays.toString(theOG), Arrays.toString(actualSortedCollection));
 	}
 
